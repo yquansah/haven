de372259520d76aa57dc8bead911674e77d500e9
Yoofi Quansah <ybquansah@gmail.com>
2024-10-10 11:19:25 -0500

    chore: use actual examples

--- dns/scripts/dig.bash
+++ dns/scripts/dig.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Look for A records of a particular domain
-dig <domain_name> A
+# Look for A records of the domain example.com
+dig example.com A
 
-# Look for MX records of a particular domain
-dig <domain_name> MX
+# Look for MX records of the domain example.com
+dig example.com MX
 
-# Reverse DNS lookup
-dig -x <ip_address>
+# Reverse DNS lookup for the IP address 192.168.1.1
+dig -x 192.168.1.1

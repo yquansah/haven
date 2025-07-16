98102c548632d3181b617889c243985e39458ed9
Yoofi Quansah <ybquansah@gmail.com>
2025-07-16 09:27:21 -0700

    feat: get ingress with traefik all the way working

--- kubernetes/self-hosted/cloud/aws/terraform/ec2.tf
+++ kubernetes/self-hosted/cloud/aws/terraform/ec2.tf
@@ -79,6 +79,15 @@ resource "aws_security_group_rule" "yke_worker_node_egress_all" {
   to_port           = 0
 }
 
+resource "aws_security_group_rule" "yke_worker_node_ingress_traefik_ingress" {
+  type              = "ingress"
+  security_group_id = aws_security_group.yke_worker_node_sg.id
+  source_security_group_id = aws_security_group.yke_elb_traefik_ingress_security_group.id
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+}
+
 resource "aws_security_group" "yke_control_plane_sg" {
   name   = "yke-control-plane-sg"
   vpc_id = aws_vpc.yke_vpc.id
--- kubernetes/self-hosted/cloud/aws/terraform/elb.tf
+++ kubernetes/self-hosted/cloud/aws/terraform/elb.tf
@@ -69,12 +69,20 @@ resource "aws_security_group" "yke_elb_traefik_ingress_security_group" {
   vpc_id = aws_vpc.yke_vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "yke_elb_traefik_ingress_kube_http" {
+resource "aws_vpc_security_group_ingress_rule" "yke_elb_traefik_ingress_kube_https" {
   security_group_id = aws_security_group.yke_elb_traefik_ingress_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "yke_elb_traefik_ingress_kube_http" {
+  security_group_id = aws_security_group.yke_elb_traefik_ingress_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "yke_elb_traefik_ingress_egress" {
